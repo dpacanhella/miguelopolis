3aaffcbfaacd9f4536416b1fde345007fda89f69
Diego Pacanhella <dpacanhella@gmail.com>
2017-06-23 15:56:28 -0300

    ajustes

--- app/src/main/java/br/dpacanhella/miguelopolis/data/api/MyFirebaseMessagingService.java
+++ app/src/main/java/br/dpacanhella/miguelopolis/data/api/MyFirebaseMessagingService.java
@@ -1,9 +1,21 @@
 package br.dpacanhella.miguelopolis.data.api;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import br.dpacanhella.miguelopolis.FarmaciaActivity;
+import br.dpacanhella.miguelopolis.R;
 
 /**
  * Created by infra on 22/06/17.
@@ -20,8 +32,46 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
     String from = remoteMessage.getFrom();
         Log.d(TAG, "Message recebido de: " + from);
 
+        final String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Token: " + token);
+
         if(remoteMessage.getNotification() != null){
             Log.d(TAG, "Notificação: " + remoteMessage.getNotification().getBody());
+
+            mostrarNotification(remoteMessage);
+        }
+
+        if(remoteMessage.getData().size() > 0){
+            Log.d(TAG, "Data: " + remoteMessage.getData());
         }
     }
+
+    //Notificação
+    private void mostrarNotification(RemoteMessage remoteMessage) {
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Intent intent = new Intent(this, FarmaciaActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,intent, PendingIntent.FLAG_ONE_SHOT);
+
+            NotificationCompat.Builder noti = new NotificationCompat.Builder(this)
+                    .setTicker("Guia Miguelópolis")
+                    .setContentTitle(remoteMessage.getData().get("Titulo"))
+                    .setContentText(remoteMessage.getData().get("Descricao"))
+                    .setWhen(System.currentTimeMillis())
+                    .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.icon_medicamentos)
+                .setLargeIcon(((BitmapDrawable) getApplicationContext().getResources().getDrawable(R.mipmap.icon_medicamentos)).getBitmap())
+                .setSound(alarmSound)
+                    .setVibrate(new long[]{100, 500, 100, 500})
+                .setContentIntent(pendingIntent);
+
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(0, noti.build());
+    }
+
+
+
 }
