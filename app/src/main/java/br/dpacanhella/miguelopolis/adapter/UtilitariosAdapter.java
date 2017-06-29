86267a3ca0a4c02e3a24ce1987b20564426c0391
Diego Pacanhella <dpacanhella@gmail.com>
2017-06-29 16:44:20 -0300

    ajustes

--- app/src/main/java/br/dpacanhella/miguelopolis/adapter/UtilitariosAdapter.java
+++ app/src/main/java/br/dpacanhella/miguelopolis/adapter/UtilitariosAdapter.java
@@ -1,5 +1,9 @@
 package br.dpacanhella.miguelopolis.adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -15,6 +21,7 @@ import java.util.List;
 import br.dpacanhella.miguelopolis.R;
 import br.dpacanhella.miguelopolis.data.model.Farmacia;
 import br.dpacanhella.miguelopolis.data.model.Utilitario;
+import br.dpacanhella.miguelopolis.util.task.AppAsyncTask;
 
 /**
  * Created by infra on 29/06/17.
@@ -23,7 +30,11 @@ import br.dpacanhella.miguelopolis.data.model.Utilitario;
 public class UtilitariosAdapter extends RecyclerView.Adapter<UtilitariosAdapter.ViewHolder> {
     List<Utilitario> utilitarioList;
     View view;
-    private ImageView image;
+    public ImageView image;
+    private LayoutInflater mLayoutInflater;
+    private float scale;
+    private int width;
+    private int height;
 
     public UtilitariosAdapter(List<Utilitario> doctorList) {
         this.utilitarioList = doctorList;
@@ -34,12 +45,38 @@ public class UtilitariosAdapter extends RecyclerView.Adapter<UtilitariosAdapter.
         view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_utilitarios_card, parent, false);
         UtilitariosAdapter.ViewHolder viewHolder = new UtilitariosAdapter.ViewHolder(view);
+
+        scale = view.getResources().getDisplayMetrics().density;
+        width = view.getResources().getDisplayMetrics().widthPixels - (int)(14 * scale + 0.5f);
+        height = (width / 16) * 9;
+
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(UtilitariosAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         holder.populate(utilitarioList.get(position));
+
+//        myViewHolder.tvModel.setText(mList.get(position).getModel());
+//        myViewHolder.tvBrand.setText(mList.get(position).getBrand());
+//
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+//            myViewHolder.ivCar.setImageResource(mList.get(position).getPhoto());
+//        }
+//        else{
+//            Bitmap bitmap = BitmapFactory.decodeResource( mContext.getResources(), mList.get(position).getPhoto());
+//            bitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+//
+//            bitmap = ImageHelper.getRoundedCornerBitmap(mContext, bitmap, 4, width, height, false, false, true, true);
+//            myViewHolder.ivCar.setImageBitmap(bitmap);
+//        }
+//
+//        try{
+//            YoYo.with(Techniques.Tada)
+//                    .duration(700)
+//                    .playOn(myViewHolder.itemView);
+//        }
+//        catch(Exception e){}
     }
 
     @Override
@@ -48,8 +85,8 @@ public class UtilitariosAdapter extends RecyclerView.Adapter<UtilitariosAdapter.
     }
 
     protected class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView txtName;
-        private TextView txtDescricao;
+        public TextView txtName;
+        public TextView txtDescricao;
         private TextView txtEndereco;
         private TextView txtTelefone;
         private TextView txtCelular;
